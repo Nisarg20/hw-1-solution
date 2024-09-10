385fcf4e0195475c860e426cb56b71a43c980146
Nisarg Doshi <ndoshi2@ncsu.edu>
2024-09-10 16:05:41 -0400

    Scaffold

---
+++ hw-1-files/coding-problems/src/main/java/hw1/problem1/Sets.java
@@ -0,0 +1,11 @@
+package hw1.problem1;
+import java.util.Set;
+public class Sets {
+    public static Set intersection(Set s1, Set s2) {
+    // Effects: Return a (non null) Set equal to the intersection of sets s1 and s2
+    // A null argument is treated as an empty set
+	// complete the implemenation below
+
+        return null;
+    }
+}
---
+++ hw-1-files/coding-problems/src/test/java/hw1/problem1/TestPairwise.java
@@ -0,0 +1,4 @@
+package hw1.problem1;
+// [TODO] use Junit and write the test cases below
+public class TestPairwise {
+}
